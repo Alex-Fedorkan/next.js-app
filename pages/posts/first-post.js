1aea6197476d8c50a3f401f3677ebac1e5adcac4
Oleksii Fedorkan <alex.fedorkan@gmail.com>
2022-11-15 21:27:54 +0200

    feat: add styles

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,12 +1,25 @@
+import Head from 'next/head';
 import Link from 'next/link';
+import Image from 'next/image';
+
+import Layout from '../../components/layout';
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
       <h1>First Post</h1>
       <h2>
         <Link href="/">Back to home</Link>
       </h2>
-    </>
+      <Image
+        src="/images/profile.jpg"
+        width={150}
+        height={200}
+        alt="Profile photo"
+      />
+    </Layout>
   );
 }
